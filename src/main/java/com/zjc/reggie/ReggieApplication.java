8753c79fdcef9c8819cf65068ebdb08b4dd6736d
jczhang <1498130271@qq.com>
2024-03-22 21:04:38 +0800

    reggie_takeout pro fail test

--- src/main/java/com/zjc/reggie/ReggieApplication.java
+++ src/main/java/com/zjc/reggie/ReggieApplication.java
@@ -16,5 +16,6 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功");
+
     }
 }
